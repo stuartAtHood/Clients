46675a8f52c97fc10b476ec989463f3e8101bf81
Stuart Hood <stuart@hoodies.co.uk>
2024-09-26 09:40:18 +0100

    Initial Commit

---
+++ Clients/ClientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientsApp.swift
+//  Clients
+//
+//  Created by Stuart Hood on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
